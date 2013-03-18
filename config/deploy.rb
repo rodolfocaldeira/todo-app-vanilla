3147392b9c2f42859249d46fae6fe50fda9d4f74
Rodolfo Caldeira <me@rodolfocaldeira.com>
2013-03-18 17:44:32 +0000

    Updated the deployment file.

--- config/deploy.rb
+++ config/deploy.rb
@@ -24,21 +24,20 @@ server conf_server, :app, :web, :db, :primary => true
 set :use_sudo, false
 set :rewrite_base, "/todo-app-vanilla/"
 
-# before "deploy:finalize_update", "copy_to_prod"
+before "deploy:finalize_update", "copy_to_prod"
 
-# desc "Copies the site to the right place"
-# task :copy_to_prod do
-#     puts "#{current_release}"
-#     run "mv #{current_release}/public/ #{current_release}/public/"
-#     run "chmod 755 #{current_release}/public/ -R"
-#
-#     htaccess = <<-EOF
-# ErrorDocument 404 /404.html
-# <IfModule mod_rewrite.c>
-#     RewriteEngine On
-#     RewriteBase #{rewrite_base}
-# </IfModule>
-# EOF
-#     put htaccess, "#{current_release}/public/.htaccess"
-#     run "chmod 755 #{current_release}/public/.htaccess"
-# end
+desc "Copies the site to the right place"
+task :copy_to_prod do
+    puts "#{current_release}"
+    run "chmod 755 #{current_release}/public/ -R"
+
+    htaccess = <<-EOF
+ErrorDocument 404 /404.html
+<IfModule mod_rewrite.c>
+    RewriteEngine On
+    RewriteBase #{rewrite_base}
+</IfModule>
+EOF
+    put htaccess, "#{current_release}/public/.htaccess"
+    run "chmod 755 #{current_release}/public/.htaccess"
+end
